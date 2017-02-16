9397301ebb554380166900a0388ded0a3c4f53f5
SaulGR <saulgr_96@hotmail.com>
2017-02-16 14:06:55 -0600

    Create Ejercicio2161.js

---
+++ Ejercicio2161.js
@@ -0,0 +1,9 @@
+var miCanvas = document.getElementById("miCanvas")
+var contexto = miCanvas.getContext("2d");
+contexto.fillStyle = "red";
+contexto.fillRect(30, 30, 50, 50);
+
+var miCanvas2 = document.getElementById("miCanvas2")
+var contexto = miCanvas2.getContext("2d");
+contexto.fillStyle = "red";
+contexto.fillRect(30, 30, 50, 50);
