2d96d574776337e1b2861af59a7a5a3289811db4
SaulGR <saulgr_96@hotmail.com>
2017-02-16 14:15:34 -0600

    Update Ejercicio2161.js

--- Ejercicio2161.js
+++ Ejercicio2161.js
@@ -6,4 +6,6 @@ contexto.fillRect(30, 30, 50, 50);
 var miCanvas2 = document.getElementById("miCanvas2")
 var contexto = miCanvas2.getContext("2d");
 contexto.fillStyle = "blue";
+contexto.fillStyle = "yellow";
+
 contexto.fillRect(60, 30, 80, 50);
