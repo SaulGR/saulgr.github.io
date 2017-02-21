e12fb24c07abc5ba46be42fbe00ab7b63b96515b
SaulGR <saulgr_96@hotmail.com>
2017-02-21 14:07:15 -0600

    Create Vertices.js

---
+++ Vertices.js
@@ -0,0 +1,6 @@
+var forma = new THREE.Geometry();
+forma.vertices.push( new THREE.Vector3( 1,  0,  1 ) ); // Vértice 0
+forma.vertices.push( new THREE.Vector3( 1,  0, -1 ) ); // Vértice 1
+forma.vertices.push( new THREE.Vector3(-1,  0, -1 ) ); // Vértice 2
+forma.vertices.push( new THREE.Vector3(-1,  0,  1 ) ); // Vértice 3
+forma.vertices.push( new THREE.Vector3( 0,  1,  0 ) ); // Vértice 4
