5f5ac5056e84416e3020fcebeff4381e77b5509c
SaulGR <saulgr_96@hotmail.com>
2017-02-28 17:55:53 -0600

    Update Peon.js

--- Peon.js
+++ Peon.js
@@ -16,7 +16,7 @@ var escena = new THREE.Scene();
 escena.add(peonMalla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 100;
 
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
