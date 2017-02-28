5a10d23bf99349d5f495ab413d2763395312be9c
SaulGR <saulgr_96@hotmail.com>
2017-02-28 14:44:02 -0600

    Update MExtrusion.js

--- MExtrusion.js
+++ MExtrusion.js
@@ -10,7 +10,7 @@ var forma = new THREE.ExtrudeGeometry( figura,
                                  
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
-malla.rotateY( Math.PI/8 );
+malla.rotateY( Math.PI/6 );
 
 var escena = new THREE.Scene();
 escena.add(malla);
