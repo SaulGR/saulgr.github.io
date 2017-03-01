3f22842ec3d42ac2bd4f225d9e9c7363125204af
SaulGR <saulgr_96@hotmail.com>
2017-02-28 18:10:46 -0600

    Create Peon2.js

---
+++ Peon2.js
@@ -0,0 +1,26 @@
+var anilloForma = new THREE.RingGeometry( 1, 5, 32 );
+var esferaForma = new THREE.SphereGeometry(.65);
+esferaForma.translate(0,20,0);
+
+var anilloMalla = new THREE.Mesh(anilloForma);
+var esferaMalla = new THREE.Mesh(esferaForma);
+anilloMalla.rotateX(Math.PI/4);
+
+
+var peonForma = new THREE.Geometry();
+peonForma.merge(anilloMalla.geometry, anilloMalla.matrix);
+peonForma.merge(esferaMalla.geometry, esferaMalla.matrix);
+
+var material = new THREE.MeshBasicMaterial( { color: 0xffff00, side: THREE.DoubleSide } );
+var peonMalla = new THREE.Mesh(peonForma, material);
+
+var escena = new THREE.Scene();
+escena.add(peonMalla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 100;
+
+renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
