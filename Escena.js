81724ef8046f11eb612a4b8ab387f81fe1c4ccb4
SaulGR <saulgr_96@hotmail.com>
2017-03-07 13:48:50 -0600

    Update Escena.js

--- Escena.js
+++ Escena.js
@@ -15,11 +15,11 @@ var cubo  = new THREE.Mesh( new THREE.BoxGeometry( 2, 2, 2 ),
 cubo.rotateY(Math.PI/4);
 
 var esfera1 = new THREE.Mesh( new THREE.SphereGeometry( 1 ),
-                              new THREE.MeshNormalMeterial() );
+                              new THREE.MeshNormalMaterial() );
 esfera1.position.x  = 5;
 
 var esfera2 = new THREE.Mesh( new THREE.SphereGeometry( 1 ),
-                              new THREE.MeshNormalMeterial() );
+                              new THREE.MeshNormalMaterial() );
 esfera1.position.x  = -5;
 esfera2.position.z  = -10;
 
