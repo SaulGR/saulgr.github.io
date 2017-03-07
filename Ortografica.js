71cc1210fe8fee69cc37f11514da366e7c2569ef
SaulGR <saulgr_96@hotmail.com>
2017-03-07 14:13:03 -0600

    Update Ortografica.js

--- Ortografica.js
+++ Ortografica.js
@@ -1,6 +1,14 @@
 var camara = new THREE.OrthographicCamera();
 
 camara.left = window.innerWidth / -2;
-camara.left = window.innerWidth / 2;
-camara.left = window.innerHeight / 2;
-camara.left = window.innerHeight / -2;
+camara.right = window.innerWidth / 2;
+camara.top = window.innerHeight / 2;
+camara.bottom = window.innerHeight / -2;
+camara.near =  0.1;
+camara.far = 1000;
+camara.updateProjectionMatrix();
+
+camara.position.z = 100;
+
+var cubo = new THREE.Mesh( new THREE.BoxGeomtery(30, 30, 30);
+                           new THREE
