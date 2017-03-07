1d6ffd57535b6c13cfc37e3e384b6d0226a5d25a
SaulGR <saulgr_96@hotmail.com>
2017-03-07 14:21:10 -0600

    Update Ortografica.js

--- Ortografica.js
+++ Ortografica.js
@@ -15,11 +15,11 @@ var cubo  = new THREE.Mesh( new THREE.BoxGeometry( 30, 30, 30 ),
          
 cubo.rotateY(Math.PI/4);
 
-var esfera1 = new THREE.Mesh( new THREE.SphereGeometry( 1 ),
+var esfera1 = new THREE.Mesh( new THREE.SphereGeometry( 15 ),
                               new THREE.MeshNormalMaterial() );
 esfera1.position.x  = 50;
 
-var esfera2 = new THREE.Mesh( new THREE.SphereGeometry( 1 ),
+var esfera2 = new THREE.Mesh( new THREE.SphereGeometry( 15 ),
                               new THREE.MeshNormalMaterial() );
 esfera2.position.x  = -50;
 esfera2.position.z  = -100;
