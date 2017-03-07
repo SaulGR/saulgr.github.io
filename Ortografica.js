a5c289b20ecbd742ffeae169ee110d3075f97a80
SaulGR <saulgr_96@hotmail.com>
2017-03-07 13:58:07 -0600

    Create Ortografica.js

---
+++ Ortografica.js
@@ -0,0 +1,6 @@
+var camara = new THREE.OrthographicCamera();
+
+camara.left = window.innerWidth / -2;
+camara.left = window.innerWidth / 2;
+camara.left = window.innerHeight / 2;
+camara.left = window.innerHeight / -2;
