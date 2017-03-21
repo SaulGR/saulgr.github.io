232a7be79f85d84900455165bbc873ba3992a1ab
SaulGR <saulgr_96@hotmail.com>
2017-03-21 14:23:41 -0600

    Update saul_21_03.js

--- saul_21_03.js
+++ saul_21_03.js
@@ -2,7 +2,7 @@ function setup(){
 escena = new THREE.Scene();
 
 camara = new THREE.PerspectiveCamera( 75,
-                                      windw.innerWidth/window.innerHeight,
+                                      window.innerWidth/window.innerHeight,
                                       0.1,
                                       1000 );
                                       
