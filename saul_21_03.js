1e542fdbccf519135d1c6e0094ef89681a835ccb
SaulGR <saulgr_96@hotmail.com>
2017-03-21 14:16:04 -0600

    Create saul_21_03.js

---
+++ saul_21_03.js
@@ -0,0 +1,30 @@
+function setup(){
+escena = new THREE.Scene();
+
+camara = new THREE.PerspectiveCamera( 75,
+                                      windw.innerWidth/window.innerHeight,
+                                      0.1,
+                                      1000 );
+                                      
+camara.position.z = 5;
+
+renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild(renderer.domElement );
+
+malla=newTHREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial());
+escena.add(malla);
+}
+
+function loop(){
+        requestAnimationFrame(loop);
+        
+        malla.rotation.x +=0.01;
+        malla.rotation.y +=0.01;
+        renderer.render(escena,camara);
+     }
+     
+     var malla, camara, renderer escena;
+     
+     setup();
+     loop();
